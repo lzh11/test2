c82ae321375d865c141e3cf56e3c7bfbaa2e3651
lzh11 <2239040999@qq.com>
2015-11-18 21:21:22 +0800

    test2
    没编完

--- test2/main.cpp
+++ test2/main.cpp
@@ -10,6 +10,17 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    int i,j,temp;
+    int N;
+    scanf("%d",&N);
+    a=(int *)malloc(N*4);
+    for(i=0;i<N;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1-i;j++)
+            
+    }
     std::cout << "Hello, World!\n";
     return 0;
 }
